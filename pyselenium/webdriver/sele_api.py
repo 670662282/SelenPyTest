b6b6eddf9a096b5805b87becfde38520e8a3d33f
Xiao Ao <xiao.ao@nexusguard.com>
2019-01-31 12:42:38 +0800

    .

--- pyselenium/webdriver/sele_api.py
+++ pyselenium/webdriver/sele_api.py
@@ -1,5 +1,4 @@
 from time import sleep, time
-from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions as EC
@@ -64,11 +63,11 @@ class ApiDriver:
     def _get_locs(*args, **kwargs):
         if len(args) == 1 or len(kwargs) == 1:
             if args and not kwargs:
-                return (LOCATORS['css'], args[0])
+                return LOCATORS['css'], args[0]
             if kwargs and not args:
                 k, v = kwargs.popitem()
                 try:
-                    return (LOCATORS[k], v)
+                    return LOCATORS[k], v
                 except KeyError:
                     raise LocationTypeError("Please use a locator：'id_' 'name' 'class_' \
                             'css' 'xpath' 'link_text' 'partial_link_text'.")
@@ -95,24 +94,16 @@ class ApiDriver:
     def script(self, script):
         self.driver.execute_script(script)
 
-    def click_button(self, name):
-        self.click_element(*self.by_text_loc(name))
-
-    def by_text_loc(self, str):
-        loc = '//span[text()="{}"]/../../..'.format(str)
-        self.logger.info(loc)
-        return (By.XPATH, loc)
-
     def click_element(self, *args, **kwargs):
         self.find_element(*args, **kwargs).click()
 
-    def send_values(self, str, *args, **kwargs):
+    def send_values(self, value, *args, **kwargs):
         obj = self.find_element(*args, **kwargs)
         try:
             obj.clear()
-        except InvalidElementStateException as e:
+        except InvalidElementStateException:
             pass
-        obj.send_keys(str)
+        obj.send_keys(value)
 
     def get_text(self, *args, **kwargs):
         return self.find_element(*args, **kwargs).text
@@ -146,58 +137,6 @@ class ApiDriver:
             except (WebDriverException, NoSuchElementException) as e:
                 self.logger.info('drap success')
                 return True
-
-    """
-    changeable select item
-    parameter: select_obj      --select element object
-    parameter: choice_value    --select choice which  ep: 0, 1, 2, 3
-    return false or choice.text
-    """
-    def select_changeable_item(self, select_obj, which):
-        choice_info = None
-        try:
-            select_obj.click()
-            you_choice = self.get_obj_list()[which]
-            choice_info = you_choice.text
-            self.logger.info('you choice is {}'.format(choice_info))
-            # wait Element change
-            sleep(0.5)
-            you_choice.click()
-        except IndexError as e:
-            self.logger.error('index is null')
-            raise
-        except StaleElementReferenceException as e:
-            # only choice oright pool result in  StaleElementReference
-            self.logger.info('Select StaleElement')
-            self.get_obj_list()[which].click()
-
-        return choice_info
-
-    """
-    const select item
-    parameter: select_obj      --select element object
-    parameter: choice_value    --select choice value
-    parameter: const_list      --select item (choosable)
-    if const_list is None ,    list of value is text what select
-    content element obj_list
-    """
-    def select_const_item(self, select_obj, choice_value, const_list=None):
-        select_obj.click()
-        # get elements list  as value
-        obj_list = self.get_obj_list()
-        # get const_list  as key
-        if const_list is None:
-            const_list = [ x.text for x in obj_list ]
-
-        my_dict = dict(zip(const_list, obj_list))
-        try:
-            self.logger.info('you choice :{}'.format(choice_value))
-            my_dict[str(choice_value)].click()
-        except KeyError as e:
-            self.logger.exception(e)
-            print("key must ", const_list)
-            select_obj.click()
-            raise e
 
 
     """
