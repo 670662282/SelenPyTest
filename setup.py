861edbc77e9a05bbd0e416030ef7352f3c2f1386
Xiao Ao <xiao.ao@nexusguard.com>
2019-01-31 12:57:11 +0800

    .

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import io
 
-with io.open("README.md.md", encoding='utf-8') as f:
+with io.open("README.md", encoding='utf-8') as f:
     long_description = f.read()
 
 install_requests = {
