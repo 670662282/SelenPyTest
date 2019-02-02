2d406b3efa9cf59f0029f8e8192929f730454a62
XiaoAO <xiao.ao@nexusguard.com>
2019-02-02 22:32:50 +0800

    update test yaml_reader

--- test/test_sele_api.py
+++ test/test_sele_api.py
@@ -1,6 +1,9 @@
+from pyselenium.configs.cfread import ReaderFactory
 from .base import SUnittest
 from pyselenium.webdriver.sele_api import ApiDriver
 from common.error import LocationTypeError
+from collections import OrderedDict
+import os
 
 
 class TestSApi(SUnittest):
@@ -10,5 +13,25 @@ class TestSApi(SUnittest):
         self.assertTupleEqual(ApiDriver._get_locs(xpath='#kw'), ('xpath', '#kw'))
         self.assertRaises(LocationTypeError, ApiDriver._get_locs, test='#kw')
 
-
-
+    def test_cfread_yaml(self):
+        file_path = 'test.yaml'
+        ya = ReaderFactory.reader(file_path)
+        info = OrderedDict({
+            'URL': "http://10.10.120.3",
+            'log': {
+                'backup': 3,
+                'level': "DEBUG",
+                "output": 2
+            },
+            'RESERVE_REPORTS_NUM': 3,
+            "EMAIL_SERVER": "smtp.163.com",
+            "EMAIL_USR": '',
+            "EMAIL_RECEIVE": '',
+            "MAIL_TITLE": 'UI自动化测试报告',
+            "IMP_TIME": 20,
+            "TIME_OUT": 30,
+        })
+        ya.data = info
+        self.assertDictEqual(info, ya.data[0])
+        if os.path.isfile(file_path):
+            os.remove(file_path)
